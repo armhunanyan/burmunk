8f90f07c7f9b2e2b43c5f1a244aeee1a1ebf959d
Arman <arman@hunanyan.am>
2021-12-23 23:49:52 +0400

    Remove old code

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -25,37 +25,4 @@ function Answer(props){
     )
 }
 
-// class Answer extends React.Component{
-//     constructor(props){
-//         super(props);
-
-//         this.state = {
-//             className:"",
-//         }
-//         this.click = this.click.bind(this);
-//     }
-//     componentWillReceiveProps(newProps){
-
-//         if(!newProps.answered){
-//             this.setState({className:''});
-//         }
-//     }
-//     click(){
-
-//         if(!this.props.answered){
-
-//             this.setState({className:"active"});
-
-//             this.props.onAnswer();
-//         }
-        
-//     }
-//     render(){
-//         var new_classname = this.state.className;
-//         return(
-//             <div className="answer" onClick={this.click}><span className={new_classname}></span>{this.props.answer}</div>
-//         )
-//     }
-// }
-
 export default Answer;
